7e0981a4df8ccffac4f8897f829d5c35a659e70a
maelstrom <maelstrom@maelstrom.dev>
2025-07-31 00:38:19 +0200

    feat: also support wafrn in new buttons
    Source:
    https://github.com/m-doescode/antlers-social/commit/55171d640e59a5424ec8865273562af01bcefd64

--- src/components/PostControls/ShareMenu/ShareMenuItems.web.tsx
+++ src/components/PostControls/ShareMenu/ShareMenuItems.web.tsx
@@ -85,11 +85,13 @@ let ShareMenuItems = ({
   }
 
   const showExternalShareButtons = useShowExternalShareButtons()
-  const isBridgyPost = !!record.bridgyOriginalUrl
-  const bridgyOriginalUrl = record.bridgyOriginalUrl as string | undefined
+  const isBridgedPost =
+    !!post.record.bridgyOriginalUrl || !!post.record.fediverseId
+  const originalPostUrl = (post.record.bridgyOriginalUrl ||
+    post.record.fediverseId) as string | undefined
 
   const onOpenOriginalPost = () => {
-    bridgyOriginalUrl && openLink(bridgyOriginalUrl, true)
+    originalPostUrl && openLink(originalPostUrl, true)
   }
 
   const onOpenPostInPdsls = () => {
@@ -113,7 +115,7 @@ let ShareMenuItems = ({
       <Menu.Outer>
         {!hideInPWI && copyLinkItem}
 
-        {showExternalShareButtons && isBridgyPost && (
+        {showExternalShareButtons && isBridgedPost && (
           <Menu.Item
             testID="postDropdownOpenOriginalPost"
             label={_(msg`Open original post`)}
