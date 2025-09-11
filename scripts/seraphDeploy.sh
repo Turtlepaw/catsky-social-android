ee40d57e4e5181cedb4713d7a3c8c4c29caf5ee2
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-09-12 01:11:06 +0800

    fix(ci): sudo on script instead
    also had to set the perms properly on the vps through nix

--- scripts/seraphDeploy.sh
+++ scripts/seraphDeploy.sh
@@ -2,4 +2,4 @@
 #!nix-shell -i bash -p go
 
 tar -xzf "/tmp/catsky/catskyweb.tar.gz" -C "/opt/catsky/" --strip-components=1 --overwrite
-systemctl restart catsky.service
+sudo systemctl restart catsky.service
